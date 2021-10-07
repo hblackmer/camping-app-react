9766cf6c1b7be9d1131e70a59ed45fa52fecf739
Helene <hcblackmer@gmail.com>
2021-10-07 13:35:39 -0700

    Added a Directory component and updated App. Rendered arrays as lists and added a unique key to each list item.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
+              <Directory />
           </div>
       );
   }
