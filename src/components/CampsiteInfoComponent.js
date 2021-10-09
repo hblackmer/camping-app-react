7e2ec3cc2e45f2b3b862b88da82271cd99bde3c5
Helene <hcblackmer@gmail.com>
2021-10-09 12:38:46 -0700

    Added comments.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -16,11 +16,34 @@ class CampsiteInfo extends React.Component {
         );
     }
 
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comments => {
+                        return (
+                            <div key={comments.id}>
+                               <div>{comments.text}</div>
+                               <div>
+                                   <span>-- {comments.author},</span>
+                                   <span>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</span>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            );
+            return <div />;
+        }
+    }
+
     render() {
         if (this.props.campsite) {
             return (
                 <div className="row">
                     {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>
             );
         }
