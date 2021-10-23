207fd935126a6d7eaa15b9ec800832e5b41cbd87
Helene <hcblackmer@gmail.com>
2021-10-23 11:38:26 -0700

    Added submit comment form.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,19 +1,84 @@
-import React, { Component } from 'react';
-import { Button } from 'reactstrap'
+import React, { Component, Fragment } from 'react';
+import { 
+    Button,
+    Label,
+    Modal, ModalHeader, ModalBody
+} from 'reactstrap'
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 class CommentForm extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            isModalOpen: false,
+          };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({ 
+            isModalOpen: !this.state.isModalOpen
+        });
+      }
+    
+    handleSubmit(values) {
+        this.toggleModal();
     }
 
     render() { 
         return (
-            <div>
-                <Button outline>
+            <Fragment>
+                <Button outline onClick={this.toggleModal}>
                     <i className="fa fa-lg fa-pencil" /> Submit Comment
                 </Button>
-            </div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <div className="form-group">
+                                <Label htmlFor="rating">Rating</Label>
+                                <Control.select 
+                                    model=".rating" 
+                                    id="rating"
+                                    name="rating"
+                                    className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                </Control.select>
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="author">Your Name</Label>
+                                <Control.text
+                                    model=".author"
+                                    id="author"
+                                    name="author"
+                                    placeholder="Your Name"
+                                    className="form-control"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <Label htmlFor="text">Comment</Label>
+                                <Control.textarea 
+                                    model=".text"
+                                    id="text"
+                                    name="text"
+                                    rows="6"
+                                    className="form-control"
+                                />
+                            </div>
+                            <Button type="submit" color="primary">
+                                Submit
+                            </Button>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </Fragment>
         );
     }
 }
